8c5e5d9d4fee4af853b32378f27ed701a8d02004
Hitarashi <sayeed205@gmail.com>
2025-09-23 11:17:07 +0530

    fix(config): change session age to 2 days
    - Update session age from '2h' to '2d' in session config

--- config/session.ts
+++ config/session.ts
@@ -1,6 +1,7 @@
-import env from '#start/env'
 import app from '@adonisjs/core/services/app'
 import { defineConfig, stores } from '@adonisjs/session'
+
+import env from '#start/env'
 
 const sessionConfig = defineConfig({
   enabled: true,
@@ -16,7 +17,7 @@ const sessionConfig = defineConfig({
    * Define how long to keep the session data alive without
    * any activity.
    */
-  age: '2h',
+  age: '2d',
 
   /**
    * Configuration for session cookie and the
